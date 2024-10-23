5456d9d2d7b6d1e4ab52ee7efba49bf2d257ae5e
ngpmnam <nguyennp@dickinson.edu>
2024-10-23 19:27:07 -0400

    Continued to write cypress test for the farm, user, and lanugage elements of the report

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.actions.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.actions.spec.js
@@ -4,8 +4,11 @@ describe('Test actions can be performed', () => {
     cy.visit("/farm/fd2-school/e2e")
 })
   it('Check the Generate Report button', () => {
-    cy.get("[data-cy = report-header]").should("not.exist")
+    //cy.get("[data-cy = report-header]").should("not.exist")
     cy.get('[data-cy = generate-report-button]').click()
     cy.get("[data-cy = report-header]").should("be.visible")
+    cy.get("[data-cy=farm-info]").should('have.text', 'Farm:Sample Farm')
+    cy.get("[data-cy=user-info]").should('contain.text', 'manager1')
+    cy.get("[data-cy=language-info]").should('have.text', 'en')
   })
 })
