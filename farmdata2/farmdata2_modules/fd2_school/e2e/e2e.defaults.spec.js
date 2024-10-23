e7f92c6eb29accfe2f3cab038919ef1c66cd41a7
ngpmnam <nguyennp@dickinson.edu>
2024-10-22 23:43:22 -0400

    cypress tests for the crop drop down

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
@@ -13,5 +13,11 @@ describe("Test the harvest report default values", () => {
     cy.get('[data-cy = end-date]').should('have.value', '2020-05-15')
   })
 
-  
+  it("Check the crop dropdown", () => {
+    cy.get('[data-cy = crop-drop-down]').children().eq(0).should('have.text', "ARUGULA")
+    cy.get('[data-cy = crop-drop-down]').children().eq(4).should('have.text', "BEAN-FAVA")
+    cy.get('[data-cy = crop-drop-down]').children().eq(110).should('have.text', "ZUCCHINI")
+    cy.get('[data-cy = crop-drop-down]').children().should('have.length', 111)
+    
+  })  
 })
