c92f31f1b52652531eec8d074dcc7c68da9fbdb8
ngpmnam <nguyennp@dickinson.edu>
2024-10-22 23:13:40 -0400

    Cypress test default start and end dates

---
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
@@ -0,0 +1,17 @@
+describe("Test the harvest report default values", () => {
+  beforeEach(() => {
+      cy.login("manager1", "farmdata2")
+      cy.visit("/farm/fd2-school/e2e")
+  })
+    
+  it("Check the page header", () => {
+    cy.get("[data-cy=page-header]").should("have.text","Harvest Report")
+  })
+
+  it("Check the default start and end dates", () => {
+    cy.get('[data-cy = start-date]').should('have.value', '2020-05-05')
+    cy.get('[data-cy = end-date]').should('have.value', '2020-05-15')
+  })
+
+  
+})
